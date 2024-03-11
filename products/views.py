6b056bb134e2b382eaa1812b1a5de7a41c4ca312
jinitha <jinithakg@gmail.com>
2024-03-11 17:23:06 +0530

    first level completed

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def IndexView(request):
+    return render(request, 'index.html')
+
+def ProductListView(request):
+    return render(request,'products.html')
+
+def ProductDetailView(request):
+    return render(request, 'products_detal.html')
+
