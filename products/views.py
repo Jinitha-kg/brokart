78fb526450cc869e3e90d80c352e7c9060a8da56
jinitha <jinithakg@gmail.com>
2024-03-27 16:59:04 +0530

    image changed

--- products/views.py
+++ products/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-
+from . models import ProductModel
 # Create your views here.
 def IndexView(request):
     return render(request, 'index.html')
 
 def ProductListView(request):
-    return render(request,'products.html')
+    product_list=ProductModel.objects.all()
+    context={'products':product_list}
+    return render (request,'products.html', context)
 
 def ProductDetailView(request):
     return render(request, 'products_details.html')
